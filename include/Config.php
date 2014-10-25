05c40cb87aa5fb66b982daa53f1463e8cfd0d508
MuthuKumaran <muthuishere@gmail.com>
2014-10-26 00:58:06 +0530

    Initialized First version

---
+++ include/Config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'ticketdiary');
+define('DB_PASSWORD', 'Superstar1@');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ticketdiary');
+
+
+
+?>
---
+++ include/PdoHandler.php
@@ -0,0 +1,57 @@
+<?php
+
+class PdoHandler {
+
+	private $db;
+
+    function __construct() {
+        
+		   require_once dirname(__FILE__) . '/Config.php';
+     require_once dirname(__FILE__) . '/class.db.php';
+	   
+	   //$this->db = Database::obtain(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME); 
+	   
+	     $this->db = new db("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
+
+// connect to the server 
+ //$this->db->connect(); 
+ 
+ 
+    }
+	
+	function close(){
+	
+	 $this->db->close();
+	
+	}
+	
+	
+	 	public function getStation($code) {
+	
+	
+			$bind = array(
+				":code" => "$code"
+			);
+		
+			
+			$results = $this->db->run("SELECT code,name,location,totaltrains FROM stations WHERE code=:code", $bind);
+
+			if(count($results) > 0)
+				return $results[0];
+			else
+				return NULL;
+		
+			//return $results;
+
+     
+    }
+	
+ 
+	   
+	  
+
+	
+}
+
+
+?>
